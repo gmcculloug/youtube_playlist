a49bb47898a842cf5a9ada6fa6777adae917a019
Greg McCullough <gmccullo@redhat.com>
2024-12-31 14:09:33 -0500

    Evaluate additional results to determine if a match was found Add DEBUG_MODE flag for testing without wasting Youtube API quota

--- create_youtube_playlisst.py
+++ create_youtube_playlisst.py
@@ -8,11 +8,12 @@ import sys
 from youtube import YouTube
 from thefuzz import process
 
+DEBUG_MODE = True
 TARGET_PLAYLIST_NAME = 'Test'
 TOKEN_FILE = 'token.json'
 SONG_LIST_INPUT_FILE = 'song_list.txt'
 
-def get_song_list(yt):
+def get_master_song_list(yt):
   master_playlists = find_master_playlists(yt)
   return collect_playlist_items(yt, master_playlists)
 
@@ -37,7 +38,11 @@ def collect_playlist_items(yt, playlists):
   return playlist_items
 
 def song_title_cleanup(song_title):
-  return song_title.replace('(', '').replace(')', '').replace('’', '').replace('\'', '')
+  return song_title.translate(song_title.maketrans({
+    '(': '', 
+    ')': '', 
+    '’': '',
+    '\'': ''}))
 
 def read_song_list_from_file():
   with open(SONG_LIST_INPUT_FILE, 'r') as f:
@@ -49,7 +54,29 @@ def read_song_list_from_file():
   # Remove any punctuation
   song_list = [song_title_cleanup(item) for item in song_list]
 
+  print(f"Processing {len(song_list)} songs from file")
+
   return song_list
+
+def find_matching_video(song_name, master_song_list):
+  matches = process.extract(song_name, master_song_list.keys())
+  if not matches:
+    print(f"No matches found for {song_name}")
+    return None
+
+  for match in matches:
+    video_title_match = str(match[0])
+
+    # Print only if the song name is not part of the video title
+    if song_name.lower() in song_title_cleanup(video_title_match).lower():
+      return master_song_list[video_title_match]
+    else:
+      if DEBUG_MODE:
+        print(f"Song name not in video title: {song_name}, {video_title_match}")
+
+  print(f"No matches found for {song_name}")
+  return None
+
 
 def main():
   song_list = read_song_list_from_file()
@@ -57,28 +84,19 @@ def main():
   yt = YouTube(TOKEN_FILE)
   yt.init_service()
 
-  master_song_list = get_song_list(yt)
+  master_song_list = get_master_song_list(yt)
 
   playlist = yt.find_or_create_playlist(TARGET_PLAYLIST_NAME, privacy_status='private')
 
   for song_name in song_list:
-    matches = process.extract(song_name, master_song_list.keys())
-    if not matches:
-      print(f"No matches found for {song_name}")
+    video = find_matching_video(song_name, master_song_list)
+    if not video:
+      print(f"Could not find matching video for: {song_name}")
       continue
-
-    video_title_match = str(matches[0][0])
-
-    # Print only if the song name is not part of the video title
-    if song_name.lower() not in song_title_cleanup(video_title_match).lower():
-      print(f"No match found for: {song_name}")
-      for match in matches:
-        print(f"Matched Video: {match[0]} ({match[1]})")
-      print()
     else:
-      video_id = master_song_list[video_title_match]["snippet"]["resourceId"]["videoId"]
-      yt.add_video_to_playlist(playlist["id"], video_id)
-
+      video_id = video["snippet"]["resourceId"]["videoId"]
+      if not DEBUG_MODE:
+        yt.add_video_to_playlist(playlist["id"], video_id)
 
 if __name__ == "__main__":
-    main()
+  main()
